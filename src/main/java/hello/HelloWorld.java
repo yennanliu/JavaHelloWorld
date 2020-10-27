13750418513007eaabdb7c979c23ee09a0037ae6
yennanliu <f339339@gmail.com>
2020-10-27 19:28:32 +0800

    update pom.xml, add HelloWorld2.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package hello;
 public class HelloWorld {
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
+    System.out.println("HelloWorld !!!");
     System.out.println(greeter.sayHello());
   }
 }
