b0a98c8f3a3a5786e935f039e1a80b73844783ed
yennanliu <f339339@gmail.com>
2020-10-28 00:22:20 +0800

    add src/main/java/dev/, update .gitignore

---
+++ src/main/java/dev/ForLoop.java
@@ -0,0 +1,10 @@
+package dev;
+
+public class ForLoop {
+    public static void main(String[] args){
+        System.out.println("ForLoop !!!");
+        for (int i = 0; i <10; i++){
+            System.out.println(i);
+        }
+    }
+}
