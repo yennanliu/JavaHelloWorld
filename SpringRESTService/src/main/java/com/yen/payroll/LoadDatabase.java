a91739f94bf51b624ea2ff03c6e240e0ca67c430
yennanliu <f339339@gmail.com>
2020-11-20 10:44:01 +0800

    add payroll/LoadDatabase.java, payroll/PayrollApplication.java

---
+++ SpringRESTService/src/main/java/com/yen/payroll/LoadDatabase.java
@@ -0,0 +1,26 @@
+package com.yen.payroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(LoadDatabase.class);
+
+    // Spring Boot will run ALL CommandLineRunner beans once the application context is loaded.
+    // This runner will request a copy of the EmployeeRepository you just created.
+    // Using it, it will create two entities and store them.
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository){
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("JIM JACK", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("FRIDDIE", "thief")));
+        };
+    }
+}
