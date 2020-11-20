4956b12ac52d70fd1d14dc3236a122490d5ef648
yennj12 <f339339@gmail.com>
2020-11-20 11:45:05 +0800

    Update EmployeeRepository.java

--- SpringRESTService/src/main/java/com/yen/payroll/EmployeeRepository.java
+++ SpringRESTService/src/main/java/com/yen/payroll/EmployeeRepository.java
@@ -1,15 +1,14 @@
 package com.yen.payroll;
 
-// 	There are multiple repository implementations besides JPA. You can use Spring Data MongoDB, Spring Data GemFire, Spring Data Cassandra, etc. For this tutorial, we’ll stick with JPA.
+// There are multiple repository implementations besides JPA. You can use Spring Data MongoDB, Spring Data GemFire, Spring Data Cassandra, etc. For this tutorial, we’ll stick with JPA.
 
 /*
-* This interface extends Spring Data JPA’s JpaRepository,
-* specifying the domain type as Employee and the id type as Long.
-* This interface, though empty on the surface, packs a punch given it supports:
-*  - Creating new instances
-*  - Updating existing ones
-*  - Deleting
-*  - Finding (one, all, by simple or complex properties)
+*  This interface extends Spring Data JPA’s JpaRepository, specifying the domain type as Employee and the id type as Long.
+*  This interface, though empty on the surface, packs a punch given it supports:
+*     - Creating new instances
+*     - Updating existing ones
+*     - Deleting
+*     - Finding (one, all, by simple or complex properties)
  */
 import org.springframework.data.jpa.repository.JpaRepository;
 
