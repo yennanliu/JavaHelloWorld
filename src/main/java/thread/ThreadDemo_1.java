c8122f38a42754e7bfabd082630bf226109f0ab8
yennanliu <f339339@gmail.com>
2020-11-21 15:17:51 +0800

    add java/thread/ demo

---
+++ src/main/java/thread/ThreadDemo_1.java
@@ -0,0 +1,29 @@
+package thread;
+
+class MyThread1 extends Thread{
+    public MyThread1(String name){
+        super(name);
+    }
+
+    // re-write run method in Thread
+    public void run(){
+        int i = 0;
+        while (i++ < 10){
+            System.out.println(Thread.currentThread().getName() +
+            "'s run() method is running");
+        }
+    }
+}
+
+public class ThreadDemo_1 {
+    public static void main(String[] args){
+        MyThread1 thread1 = new MyThread1("thread1");
+
+        // use start method to start the thread
+        thread1.start();
+
+        // create the othere thread
+        MyThread1 thread2 = new MyThread1("thread2");
+        thread2.start();
+    }
+}
