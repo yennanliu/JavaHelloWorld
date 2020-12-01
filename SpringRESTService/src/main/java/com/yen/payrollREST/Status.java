d25930cd2bf46924e3865d3943de98c8db419410
yennanliu <f339339@gmail.com>
2020-12-01 23:21:03 +0800

    add Order.java, Status.java

---
+++ SpringRESTService/src/main/java/com/yen/payrollREST/Status.java
@@ -0,0 +1,8 @@
+package com.yen.payrollREST;
+
+// This enum captures the various states an Order can occupy. For this tutorial, let’s keep it simple.
+enum Status {
+    IN_PROGRESS, //
+    COMPLETED, //
+    CANCELLED
+}
