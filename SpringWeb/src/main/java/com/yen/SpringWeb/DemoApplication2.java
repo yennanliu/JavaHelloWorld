a5082fa824a0c6345d717eced19151ff9507359e
yennanliu <f339339@gmail.com>
2020-12-30 15:52:15 +0800

    add /main/resources/templates/, update DemoApplication2.java, fix pom.xml format

--- SpringWeb/src/main/java/com/yen/SpringWeb/DemoApplication2.java
+++ SpringWeb/src/main/java/com/yen/SpringWeb/DemoApplication2.java
@@ -3,13 +3,13 @@ package com.yen.SpringWeb;
 // https://ithelp.ithome.com.tw/articles/10193631
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.Controller;
+//import org.springframework.web.servlet.mvc.Controller;
 
+// TODO : fix why can not use @Controller in the project
 @RestController
 public class DemoApplication2 {
     public static void main(String[] args) {
@@ -17,8 +17,8 @@ public class DemoApplication2 {
     }
 
     @RequestMapping("/MyFirstPage")
-    public String greeting(@RequestParam(value="title", required=false, defaultValue="xiao") String title, Model model) {
+    public String greeting(@RequestParam(value="title", required=false, defaultValue="HI THIS IS MY JAVA WEB PAGE") String title, Model model) {
         model.addAttribute("name", title);
-        return "index";
+        return "HI THIS IS MY JAVA WEB PAGE";
     }
 }
