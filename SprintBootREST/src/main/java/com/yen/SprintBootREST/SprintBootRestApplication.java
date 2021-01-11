9a710f21ff9098be134ef609ea0de03c5c5a22df
yennanliu <f339339@gmail.com>
2021-01-11 15:58:17 +0800

    add /SprintBootREST

---
+++ SprintBootREST/src/main/java/com/yen/SprintBootREST/SprintBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SprintBootREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootRestApplication.class, args);
+	}
+
+}
