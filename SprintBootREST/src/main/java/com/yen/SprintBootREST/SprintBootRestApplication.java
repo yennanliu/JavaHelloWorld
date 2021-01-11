dc43edb429f68289bbaa607c21410c636cec235a
yennanliu <f339339@gmail.com>
2021-01-11 16:58:18 +0800

    add controller/DeleteAPI.java, controller/PutAPI.java, update SprintBootRestApplication.java

--- SprintBootREST/src/main/java/com/yen/SprintBootREST/SprintBootRestApplication.java
+++ SprintBootREST/src/main/java/com/yen/SprintBootREST/SprintBootRestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SprintBootRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SprintBootRestApplication.class, args);
 	}
-
 }
---
+++ SprintBootREST/src/main/java/com/yen/controller/DeleteAPI.java
@@ -0,0 +1,24 @@
+package com.yen.controller;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yen.model.Product;
+
+@RestController
+public class DeleteAPI {
+    private static Map<String, Product> productRepo = new HashMap<>();
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted OK !", HttpStatus.OK);
+    }
+}
