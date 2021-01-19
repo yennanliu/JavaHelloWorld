ee7c0f62bc2af8edd30dcebd6c5a4b5df4afa6aa
yennanliu <f339339@gmail.com>
2021-01-19 22:56:30 +0800

    add SprintBootTravelAPI/

---
+++ SprintBootTravelAPI/src/main/java/com/yen/SprintBootTravelAPI/SprintBootTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SprintBootTravelAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootTravelApiApplication.class, args);
+	}
+
+}
---
+++ SprintBootTravelAPI/src/test/java/com/yen/SprintBootTravelAPI/SprintBootTravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.SprintBootTravelAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
