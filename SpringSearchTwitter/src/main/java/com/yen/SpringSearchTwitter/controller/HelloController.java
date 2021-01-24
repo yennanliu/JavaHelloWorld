9c415a72f2917377ee9798ffc2978a45ce9c936c
yennanliu <f339339@gmail.com>
2021-01-24 19:07:47 +0800

    update constoller to get name as url input

--- SpringSearchTwitter/src/main/java/com/yen/SpringSearchTwitter/controller/HelloController.java
+++ SpringSearchTwitter/src/main/java/com/yen/SpringSearchTwitter/controller/HelloController.java
@@ -3,12 +3,19 @@ package com.yen.SpringSearchTwitter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @RequestMapping("/")
     public String hello(Model model){
         model.addAttribute("message", "hello from the controller !!!");
+        return "resultPage";
+    }
+
+    @RequestMapping("/username/")
+    public String hello2(@RequestParam("name") String userName, Model model){
+        model.addAttribute("message", "hello,  " + userName);
         return "resultPage";
     }
 }
