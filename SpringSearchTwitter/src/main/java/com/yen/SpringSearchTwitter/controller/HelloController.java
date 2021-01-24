1b4aa3aaa5a0484e5766702e89fc8c7765fa4fd9
yennanliu <f339339@gmail.com>
2021-01-24 18:59:08 +0800

    update update templates/resultPage.html and load msg from controller.java

--- SpringSearchTwitter/src/main/java/com/yen/SpringSearchTwitter/controller/HelloController.java
+++ SpringSearchTwitter/src/main/java/com/yen/SpringSearchTwitter/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.yen.SpringSearchTwitter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
     @RequestMapping("/")
-    public String hello(){
+    public String hello(Model model){
+        model.addAttribute("message", "hello from the controller !!!");
         return "resultPage";
     }
 }
