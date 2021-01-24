7a2e7f2bbde2bd3fc8f91e66e14273c17837c1f9
yennanliu <f339339@gmail.com>
2021-01-24 18:27:57 +0800

    add SpringSearchTwitter/

---
+++ SpringSearchTwitter/src/main/java/com/yen/SpringSearchTwitter/SpringSearchTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SpringSearchTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSearchTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSearchTwitterApplication.class, args);
+	}
+
+}
