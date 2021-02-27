282e92a8576ad430b6a8ad1cf65cd8b0b6971eae
yennanliu <f339339@gmail.com>
2021-02-27 13:11:25 +0800

    add project SpringTwitter/

---
+++ SpringTwitter/src/main/java/com/yen/tweet/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.yen.tweet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+    @RequestMapping("/test1")
+    public String hello(Model model){
+        model.addAttribute("message", "hello from the controller !!!");
+        return "resultPage";
+    }
+
+    @RequestMapping("/test2")
+    public String hello2(Model model){
+        model.addAttribute("message", "hello  2 from the controller !!!");
+        return "resultPage";
+    }
+}
