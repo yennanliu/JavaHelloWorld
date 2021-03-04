2d0dbbf9de4e4f38b56995a7fb57848e5bd0c0d8
yennanliu <f339339@gmail.com>
2021-03-05 07:42:54 +0800

    update code, add pic

--- README.md
+++ README.md
@@ -15,6 +15,7 @@
         - [Class Demo1](./src/main/java/Basics/ClassDemo1.java)
         - [Class Demo2](./src/main/java/Basics/ClassDemo2.java)
         - [Attr VS Local Variable1 Demo1](./src/main/java/Basics/AttrVSLocalVariable1.java)
+<p ><img src ="https://github.com/yennanliu/JavaHelloWorld/blob/main/doc/pic/ClassDemo2.svg"></p>
 <p ><img src ="https://github.com/yennanliu/JavaHelloWorld/blob/main/doc/pic/class_in_memory.svg"></p>
 
 - Array1D:
--- src/main/java/Basics/ClassDemo2.java
+++ src/main/java/Basics/ClassDemo2.java
@@ -2,7 +2,6 @@ package Basics;
 
 // https://www.youtube.com/watch?v=hJXPcZa698A&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=189
 
-
 public class ClassDemo2 {
     public static void main(String[] args){
         // run
@@ -17,5 +16,12 @@ public class ClassDemo2 {
         p1.addAge(100);
 
         System.out.println(p1.age);
+
+        System.out.println("===============");
+
+        Person_ p2 = new Person_();
+        p2.showAge();
+        p2.addAge(10);
+        p2.showAge();
     }
 }
