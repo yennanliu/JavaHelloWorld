602cef4656aa1f83a391afae2c998c6b4967c8b5
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-03-14 18:25:31 +0800

    fix return page ref

--- SpringTwitter/src/main/java/com/yen/tweet/controller/TestController.java
+++ SpringTwitter/src/main/java/com/yen/tweet/controller/TestController.java
@@ -10,12 +10,12 @@ public class TestController {
     @RequestMapping("/test1")
     public String hello(Model model){
         model.addAttribute("message", "hello from the controller !!!");
-        return "resultPage";
+        return "testPage";
     }
 
     @RequestMapping("/test2")
     public String hello2(Model model){
         model.addAttribute("message", "hello  2 from the controller !!!");
-        return "resultPage";
+        return "testPage";
     }
 }
