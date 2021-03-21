704040acb38467afbef84193fcc15c3c6697ebd4
yennanliu <f339339@gmail.com>
2021-03-21 15:06:27 +0800

    update valueTransfer1.java

--- src/main/java/Basics/valueTransfer1.java
+++ src/main/java/Basics/valueTransfer1.java
@@ -2,6 +2,8 @@ package Basics;
 
 // plz compare with valueTransfer2
 
+import java.util.Arrays;
+
 public class valueTransfer1 {
     public static void main(String[] args){
         // run
@@ -18,6 +20,14 @@ public class valueTransfer1 {
         // and the m, n inside swap_1 method are actually in "stack" space, so they r not in the heap space, as the one we defined at beginning
         // so the swap op is not working here
         System.out.println("m = " + m + ", n = " + n);
+
+        System.out.println("--------------");
+
+        int arr[] = {1,3};
+        test.swap_1_fixed(arr);
+        for (int j = 0; j < arr.length ; j ++){
+            System.out.println(arr[j]);
+        }
     }
 
     public void swap_1(int m, int n) {
@@ -25,5 +35,12 @@ public class valueTransfer1 {
         int tmp_ = m;
         m = n;
         n = tmp_;
+    }
+
+    // let's define a "fixed" version here
+    public void swap_1_fixed(int[] arr){
+        int tmp_ = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp_;
     }
 }
