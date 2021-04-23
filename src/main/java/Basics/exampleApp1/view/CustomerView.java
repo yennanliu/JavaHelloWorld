afcc9b55ab440ee6ce8ed16a59930e91ae9ab557
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-04-23 18:55:58 +0800

    implement/update exampleApp1/view/CustomerView.java

--- src/main/java/Basics/exampleApp1/view/CustomerView.java
+++ src/main/java/Basics/exampleApp1/view/CustomerView.java
@@ -2,17 +2,30 @@ package Basics.exampleApp1.view;
 
 // https://www.youtube.com/watch?v=c2d0vVbvoSQ&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=248
 // https://www.youtube.com/watch?v=KyQNP7_6DUM&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=252
+// https://www.youtube.com/watch?v=wWzlY8i8klE&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=254
 
 /*
  *   CustomerView
  *    -> for user interface, user data for client side...
  */
 
+import Basics.exampleApp1.bean.Customer;
 import Basics.exampleApp1.service.CustomerList;
 import Basics.exampleApp1.utils.CMUtility;
 
 public class CustomerView {
     private CustomerList customerList = new CustomerList(10);
+
+    // constructor
+
+    // init with some default customers here for testing
+    public CustomerView(){
+        Customer customer1 = new Customer("king", 'g', 19, "0987654321", "king@fb.com" );
+        Customer customer2 = new Customer("blue.mary", 'g', 31, "09877721", "blue.mary@fb.com" );
+
+        customerList.addCustomer(customer1);
+        customerList.addCustomer(customer2);
+    }
 
     //method
     /***
@@ -87,10 +100,28 @@ public class CustomerView {
      *  list all customer
      */
     private void listAllCustomer(){
-        System.out.println("listAllCustomer");
+        //System.out.println("listAllCustomer");
+        System.out.println("--------------------- Customer List --------------------- \n");
+        int total = customerList.getTotal();
+
+        if (total == 0){
+            System.out.println("no any customer !");
+        } else{
+            System.out.println("id\tname\tsex\tgender\tphone\t\temail");
+            Customer[] custs =  customerList.getAllCustomers();
+            for (int i=0; i < custs.length; i ++){
+                Customer cust = custs[i];
+                System.out.println(
+                        (i+1) + "\t" + cust.getName() + "\t" + cust.getGender() + "\t" + cust.getPhone() + "\t" + cust.getEmail()
+                );
+            }
+        }
+
+        System.out.println("--------------------- Customer List End --------------------- \n");
 
     };
 
+    /** entry point */
     public static void main(String[] args){
         CustomerView customerView = new CustomerView();
         customerView.enterMainMenu();
