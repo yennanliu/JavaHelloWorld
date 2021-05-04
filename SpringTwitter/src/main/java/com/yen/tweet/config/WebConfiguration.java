ffaea9ba1fa3e1373fa2c7a4ad86c3aaacb47f01
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-05-04 18:34:39 +0800

    add swagger-ui to pom.xml, update it to webConfiguration,

--- SpringTwitter/src/main/java/com/yen/tweet/config/WebConfiguration.java
+++ SpringTwitter/src/main/java/com/yen/tweet/config/WebConfiguration.java
@@ -1,7 +1,8 @@
 package com.yen.tweet.config;
 
-// p.67 p.76 p.109
+// p.67 p.76 p.109 p.147
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -11,10 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 // UDF
 import com.yen.tweet.date.USLocalDateFormatter;
 import org.springframework.web.util.UrlPathHelper;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override public void addFormatters(FormatterRegistry registry){
         registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
@@ -26,5 +31,13 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         configurer.setUrlPathHelper(urlPathHelper);
         configurer.setUseSuffixPatternMatch(true);
         configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+
+    @Bean
+    public Docket userApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(path -> path.startsWith("/api/"))
+                .build();
     }
 }
