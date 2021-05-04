a90ef005e92795f895eaa90eb2b615dbadc42701
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-05-04 19:05:43 +0800

    add tweet/error/EntityNotFoundMapper.java

---
+++ SpringTwitter/src/main/java/com/yen/tweet/error/EntityNotFoundMapper.java
@@ -0,0 +1,17 @@
+package com.yen.tweet.error;
+
+// p.145
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EntityNotFoundMapper {
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entity could not be found")
+    public void handleNotFound(){
+
+    }
+}
