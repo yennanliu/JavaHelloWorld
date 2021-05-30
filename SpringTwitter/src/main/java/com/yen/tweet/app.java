c3ef2102daec3ce096dad28dd433939f0367b020
yennanliu <f339339@gmail.com>
2021-05-30 17:30:08 +0800

    update materializecss version, fix code format, progress

--- SpringTwitter/src/main/java/com/yen/tweet/app.java
+++ SpringTwitter/src/main/java/com/yen/tweet/app.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
 // either add to ComponentScan or update config, so java spring can scan the needed class, controller ....
 // https://stackoverflow.com/questions/40384056/consider-defining-a-bean-of-type-package-in-your-configuration-spring-boot
+@SpringBootApplication
 @ComponentScan({"com.yen.tweet.service", "com.yen.tweet.controller", "com.yen.tweet.search", "com.yen.tweet.api", "com.yen.tweet.user"}) // modify the spring component scan area : https://stackoverflow.com/questions/40384056/consider-defining-a-bean-of-type-package-in-your-configuration-spring-boot
 public class app{
+
     public static void main(String[] args) {
         SpringApplication.run(app.class, args);
     }
