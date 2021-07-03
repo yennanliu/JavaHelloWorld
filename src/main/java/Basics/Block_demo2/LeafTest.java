e0ccd4f2694e8215cd9d36cb12ad3d0adef9118d
yennanliu <f339339@gmail.com>
2021-07-03 10:33:23 +0800

    update (biock demo2

--- src/main/java/Basics/Block_demo2/LeafTest.java
+++ src/main/java/Basics/Block_demo2/LeafTest.java
@@ -2,41 +2,56 @@ package Basics.Block_demo2;
 
 // https://www.youtube.com/watch?v=SvCdVd9que4&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=331
 
+/**
+ *  Summary
+ *
+ *  1) SUPER CLASS -> SUB CLASS
+ *  2) STATIC FIRST
+ */
+
+
 // run
 public class LeafTest{
     public static void main(String[] args) {
         // run
         new Leaf();
-
+        System.out.println("================");
+        new Leaf();
     }
 }
 
 class Root{
+    // static code block
     static {
         System.out.println("Root's static code block");
     }
 
+    // code block
     {
         System.out.println("Root's code block");
     }
 
     // constructor
     public Root(){
+        super();
         System.out.println("Root 's non param constructor");
     }
 }
 
 class Mid extends Root {
+    // static code block
     static {
         System.out.println("Mid's static code block");
     }
 
+    // code block
     {
         System.out.println("Mid's code block");
     }
 
     // constructor
     public Mid(){
+        super();
         System.out.println("Mid 's non param constructor");
     }
 
@@ -49,10 +64,12 @@ class Mid extends Root {
 }
 
 class Leaf extends Mid {
+    // static code block
     static {
         System.out.println("LeafTest's static code block");
     }
 
+    // code block
     {
         System.out.println("LeafTest's code block");
     }
@@ -60,6 +77,7 @@ class Leaf extends Mid {
     // constructor
     public Leaf(){
         // via super to call param constructor in SUPER CLASS
+        //   -> will call Mid(String msg) first
         super("UCLA");
         System.out.println("LeafTest 's non param constructor");
     }
