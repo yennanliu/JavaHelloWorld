10c4fb309e5c53bc1c5249dfbf69a70b846f1fb1
yennanliu <f339339@gmail.com>
2021-07-03 11:30:44 +0800

    update final demo1

--- src/main/java/Basics/FinalDemo1.java
+++ src/main/java/Basics/FinalDemo1.java
@@ -5,7 +5,7 @@ package Basics;
 /**
  *  Final
  *
- *  1) Can decorate : class, method, attr
+ *  1) Can decorate : class, method, attr (variable)
  *  2) Final class
  *     2.1) CAN'T BE EXTENDED (no sub class)
  *           -> example : String, System, StringBuffer
@@ -13,10 +13,51 @@ package Basics;
  *  3) Final method
  *      3.1) CAN'T BE OVERRIDDEN (no overwrite method)
  *           -> example : getClass() method in Object class
+ *
+ *  4) Final attr (variable)
+ *      4.1) attr/variable becomes CONSTANT (unchanged)
+ *      4.2) final attr
+ *          -> can be used in
+ *              - explicitly declaration
+ *              - initialize in code block
+ *              - initialize in constructor
+ *      4.3) final variable
+ *
  */
 
 public class FinalDemo1 {
+    public static void main(String[] args) {
+    }
+
+    public FinalDemo1(){
+
+        /** Final attr (variable) */
+        final int width = 10;
+        final int LEFT;
+        final int RIGHT;
+        final int Down;
+
+        // wrong : Cannot assign a value to final variable 'width'
+        //public void change(){
+        //    width = 20;
+        //}
+
+        {
+            LEFT = 2;
+        }
+
+        RIGHT = 2;
+    }
+
+//    public FinalDemo1(int n){
+//        RIGHT = n;
+//    }
+
+//    public void setDown(int down){
+//        this.Down = down;
+//    }
 }
+
 
 /** Final class */
 
