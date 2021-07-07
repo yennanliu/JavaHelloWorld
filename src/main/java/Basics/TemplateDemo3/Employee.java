3b681ca2b0914988fe2714b15ef8d6ef05b9ba22
yennanliu <f339339@gmail.com>
2021-07-08 06:35:10 +0800

    add TemplateDemo3/

---
+++ src/main/java/Basics/TemplateDemo3/Employee.java
@@ -0,0 +1,48 @@
+package Basics.TemplateDemo3;
+
+// https://www.youtube.com/watch?v=3VqFJPmA3wg&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=348
+
+public abstract class Employee {
+
+    //attr
+    private String name;
+    private int number;
+    private MyDate birthday;
+
+    // getter, setter
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public MyDate getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setBirthday(MyDate birthday) {
+        this.birthday = birthday;
+    }
+
+    // method
+    public abstract void earnings();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
---
+++ src/main/java/Basics/TemplateDemo3/MyDate.java
@@ -0,0 +1,53 @@
+package Basics.TemplateDemo3;
+
+// https://www.youtube.com/watch?v=3VqFJPmA3wg&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=348
+
+public class MyDate {
+
+    //attr
+    private int year;
+    private int month;
+    private int day;
+
+    // constructor
+    public MyDate(){
+
+    }
+
+    public MyDate(int year, int month, int day){
+        super();
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    // getter, setter
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    // method
+    public String toDateString(){
+        return year + "year " + month + " month " + day + "day";
+    }
+}
