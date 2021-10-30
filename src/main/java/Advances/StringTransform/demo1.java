0492a8c654e3c0df716c9722e4e53dffe2d2ba28
yennanliu <f339339@gmail.com>
2021-10-30 09:54:23 +0800

    update StringTransform/demo1.java

--- src/main/java/Advances/StringTransform/demo1.java
+++ src/main/java/Advances/StringTransform/demo1.java
@@ -5,6 +5,8 @@ package Advances.StringTransform;
 /**
  *   String class <---> other class Demo 1
  *
+ *   1) String <--> Integer
+ *   2) String <--> char[]
  */
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.Test;
 public class demo1 {
 
     /**
+     *  (review)
      *  1) String -> basic data type, wrapper class (static method): parseXXX(str)
      *  2) basic data type, wrapper class -> String : String override valueOf(xxx)
      */
@@ -22,7 +25,29 @@ public class demo1 {
         int num1 = Integer.parseInt(str1);
         System.out.println(num1);
 
+        System.out.println("================");
+
         String str1_ =  String.valueOf(num1);
         System.out.println(str1_);
+    }
+
+    /**
+     *   String <---> char[]
+     *   1) String --> char[] : String's toCharArray method
+     *   2) char[] --> String : via String's constructor
+     */
+    @Test
+    public void test2(){
+        String s1 = "abc123";
+        char[] charArray1 = s1.toCharArray();
+        for (int i=0; i < charArray1.length; i++){
+            System.out.println(charArray1[i]);
+        }
+
+        System.out.println("================");
+
+        char[] arr2 = new char[]{'x','y','z'};
+        String s2 = new String(arr2);
+        System.out.println(s2);
     }
 }
