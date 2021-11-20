219bdb531714a04f8f6271fbf64fdb35018cd5c0
yennanliu <f339339@gmail.com>
2021-11-20 10:44:01 +0800

    update Advances/ComparableComparator demo

--- src/main/java/Advances/ComparableComparator/demo1.java
+++ src/main/java/Advances/ComparableComparator/demo1.java
@@ -1,6 +1,7 @@
 package Advances.ComparableComparator;
 
 // https://www.youtube.com/watch?v=n3xOIVricqQ&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=490
+// https://www.youtube.com/watch?v=iCKc2-vslbg&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=491
 
 /**
  *  Comparable Comparator demo1
@@ -9,7 +10,22 @@ package Advances.ComparableComparator;
  *     . However, in some cases, we need to ordering (compare) the instances.
  *     -> `Comparable`, and `Comparator` are the interface for that
  *
- *  2) Comparable interface
+ *  2) Comparable interface demo (natural ordering)
+ *
+ *    2-1) String implements Comparable interface, override compareTo() method
+ *      -> so it can compare instances (> or <)
+ *
+ *    2-2) String, wrapper class' override compareTo()
+ *      -> will return smallest -> biggest
+ *
+ *    2-3) String, wrapper class' override compareTo() rules:
+ *      -> if current instance (this.xxx) > obj : return positive int
+ *      -> if current instance (this.xxx) < obj : return negative int
+ *      -> if current instance (this.xxx) < obj : return 0
+ *
+ *    2-4) For user-defined class, if we need to ordering it,
+ *        -> we need to implement Comparable interface, override compareTo(obj) method
+ *        -> we need to implement  `how to sorting` logic in compareTo(obj)
  *
  *  3) Comparator interface
  */
@@ -22,24 +38,29 @@ public class demo1 {
     @Test
     public void demo1(){
 
-        /**
-         *
-         *  2) Comparable interface demo
-         *
-         *    2-1) String implements Comparable interface, override compareTo() method
-         *      -> so it can compare instances (> or <)
-         *
-         *    2-2) String, wrapper class' override compareTo()
-         *      -> will return smallest -> biggest
-         *
-         *    2-3) String, wrapper class' override compareTo() rules:
-         *      -> if current instance (this.xxx) > obj : return positive int
-         *      -> if current instance (this.xxx) < obj : return negative int
-         *      -> if current instance (this.xxx) < obj : return 0
-         */
+        /** Ordering string array */
         String[] arr1 = new String[]{"dd","aa","cc","bb"};
         Arrays.sort(arr1);
 
         System.out.println(Arrays.toString(arr1)); // [aa, bb, cc, dd]
+    }
+
+    @Test
+    public void demo2(){
+        /** Ordering customized class (Goods class) (Advances.ComparableComparator.Goods.java) */
+        Goods[]arr = new Goods[4];
+        arr[0] = new Goods("iphone", 1000);
+        arr[1] = new Goods("pixel", 800);
+        arr[2] = new Goods("x11", 200);
+        arr[3] = new Goods("samsung", 100);
+
+        System.out.println(Arrays.toString(arr)); // [Good{name='iphone', price=1000.0}, Good{name='pixel', price=800.0}, Good{name='x11', price=200.0}, Good{name='samsung', price=100.0}]
+
+        // sort
+        // will case `java.lang.ClassCastException: class Advances.ComparableComparator.Goods cannot be cast to class java.lang.Comparable (Advances.ComparableComparator.Goods is in unnamed module of loader 'app'; java.lang.Comparable is in module java.base of loader 'bootstrap')` error
+        // if not implement Comparable or Comparator
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr)); // [Good{name='samsung', price=100.0}, Good{name='x11', price=200.0}, Good{name='pixel', price=800.0}, Good{name='iphone', price=1000.0}]
     }
 }
