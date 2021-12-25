fa22808e80b79fe9cfc1ad0f1823941b8abee43b
yennanliu <f339339@gmail.com>
2021-12-25 10:12:59 +0800

    update listdemo1

--- src/main/java/Advances/CollectionDemo/ListDemo1.java
+++ src/main/java/Advances/CollectionDemo/ListDemo1.java
@@ -2,6 +2,14 @@ package Advances.CollectionDemo;
 
 // https://www.youtube.com/watch?v=Y6U0PnnjNRs&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=527
 // https://www.youtube.com/watch?v=iQnIoSjxdeo&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=528
+// https://www.youtube.com/watch?v=CTvv2SEH3l4&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=529
+// https://www.youtube.com/watch?v=KptmLcdTECg&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=531
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *  List Demo1
@@ -34,7 +42,61 @@ package Advances.CollectionDemo;
  */
 
 public class ListDemo1 {
-    public static void main(String[] args) {
+    /**
+     *  List common methods
+     *
+     */
+    @Test
+    public void test1(){
+        ArrayList list1 = new ArrayList();
 
+        // add
+        list1.add(123);
+        list1.add(456);
+        list1.add("aa");
+        list1.add(new Person("tim",11));
+        list1.add(123);
+        System.out.println(list1);
+
+        // add(int index, object obj)
+        list1.add(list1);
+
+        // addAll
+        List list2 = Arrays.asList(1,2,3);
+        list1.addAll(list2);
+        System.out.println(list1);
+
+        // get
+        System.out.println(list1.get(1));
+    }
+
+    @Test
+    public void test2(){
+        ArrayList list1 = new ArrayList();
+
+        list1.add(123);
+        list1.add(456);
+        list1.add("aa");
+        list1.add(new Person("tim",11));
+        list1.add(123);
+
+        // indexOf
+        System.out.println(list1.indexOf(123));
+
+        // lastIndexOf
+        System.out.println(list1.lastIndexOf(123));
+
+        // remove
+        Object obj = list1.remove(1);
+        System.out.println("obj = " + obj);
+        System.out.println(list1);
+
+        // set
+        list1.set(0,"zz");
+        System.out.println(list1);
+
+        // subList
+        List list1_ = list1.subList(1,3);
+        System.out.println(list1_);
     }
 }
