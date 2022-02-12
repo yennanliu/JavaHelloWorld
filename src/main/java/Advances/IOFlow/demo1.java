035855bebe569902f3d3b98d7e2905058904467a
yennanliu <f339339@gmail.com>
2022-02-12 08:18:14 +0800

    update fileIO demo1

--- src/main/java/Advances/IOFlow/demo1.java
+++ src/main/java/Advances/IOFlow/demo1.java
@@ -1,6 +1,7 @@
 package Advances.IOFlow;
 
 // https://www.youtube.com/watch?v=kqG8IQNMN_s&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=584
+// https://www.youtube.com/watch?v=lms-HDCYCvQ&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=586
 
 /**
  *  IOFlow demo 1
@@ -15,14 +16,26 @@ package Advances.IOFlow;
  *   Types of I/O flow
  *
  *   1) based on unit: 字節流 (8 bit), 字符流 (16 bit)
+ *      ->  字節流 (8 bit) :  byte           : for picture, video files (non-text files)
+ *      ->  字符流 (16 bit) : char  (2 byte) : for text files
+ *
  *   2) based in flow : inputStream, outputStream
  *   3) based on roles : 節點流, 處理流
  *
+ *   4) I/O flow structure - 1
  *    abstract class  | 字節流         |  字符流
  *        輸入流       | inputStream   |  Reader
  *        輸出流       | outputStream  |  Writer
  *
  *
+ *
+ *   5) I/O flow structure - 2
+ *
+ *    abstract class  |  節點流             |  緩衝流
+ *     InputStream    |  FileInputStream   |  BufferedInputStream
+ *     OutputStream   |  FileOutputStream  |  BufferedOutputStream
+ *     Reader         |  FileReader        |  BufferedReader
+ *     Writer         |  FileWriter        |  BufferedFileWriter
  */
 
 public class demo1 {
