19f337fa717e8a707566a989856a47351fbb00ec
yennanliu <f339339@gmail.com>
2022-02-12 09:56:43 +0800

    update demo2, demo2

--- src/main/java/Advances/IOFlow/demo2.java
+++ src/main/java/Advances/IOFlow/demo2.java
@@ -19,26 +19,37 @@ public class demo2 {
      *        -> FileWriter will overwrite current content (new FileWriter(file, false);)
      *        -> FileWriter will append new data to current content (new FileWriter(file, true);)
      *
-     *
-     *
      */
     @Test
     public void test1() throws IOException {
 
-        // step 1) offer file class object, declare plan-to-write file
-        File file = new File("src/main/java/Advances/IOFlow/hello2.txt");
+        FileWriter fw = null;
 
-        // step 2) offer FileWriter object, for data writing
-        //FileWriter fw = new FileWriter(file, true); // append to current file (NOT overwrite)
-        FileWriter fw = new FileWriter(file, false); // NOT append to current file (overwrite)
+        try {
+            // step 1) offer file class object, declare plan-to-write file
+            File file = new File("src/main/java/Advances/IOFlow/hello2.txt");
 
-        // step 3) writing op
-        fw.write(">>> start >>> \n");
-        fw.write("this is Advances.IOFlow.FileWriter output!!!\n");
-        fw.write(">>> end >>> ");
+            // step 2) offer FileWriter object, for data writing
+            //FileWriter fw = new FileWriter(file, true); // append to current file (NOT overwrite)
+            fw = new FileWriter(file, false); // NOT append to current file (overwrite)
 
-        // step 4) I/O flow close (close FileWriter)
-        fw.close();
+            // step 3) writing op
+            fw.write(">>> start >>> \n");
+            fw.write("this is Advances.IOFlow.FileWriter output!!!\n");
+            fw.write(">>> end >>> ");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fw != null){
+                try{
+                    // step 4) I/O flow close (close FileWriter)
+                    fw.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
 
--- src/main/java/Advances/IOFlow/demo3.java
+++ src/main/java/Advances/IOFlow/demo3.java
@@ -2,7 +2,59 @@ package Advances.IOFlow;
 
 // https://www.youtube.com/watch?v=bgMNw70CVrw&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=589
 
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
 /**  IOFlow demo 2 : FileReader & FileWriter */
 
 public class demo3 {
+    @Test
+    public void test1() throws IOException {
+
+        FileReader fr = null;
+        FileWriter fw = null;
+
+        try {
+
+            // step 1) create file instance, define read-in and write-out file
+            File srcFile = new File("src/main/java/Advances/IOFlow/hello.txt");
+            File destFile = new File("src/main/java/Advances/IOFlow/hello3.txt");
+
+            // step 2) create input flow, and output flow instance
+            fr =  new FileReader(srcFile);
+            fw = new FileWriter(destFile);
+
+            // step 3) data read-in and write out op
+            char[] cbuf = new char[5];
+            int len; // record read-in char number (to cbuf) every time
+            while ((len = fr.read(cbuf))!= -1){
+                // NOTE !!! write len char each time
+                fw.write(cbuf, 0, len);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            // step 4) close down IO flow resources
+
+            // close fr
+            if (fr != null){
+                try{
+                    fr.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            // close fw
+            if (fw != null){
+                try{
+                    fw.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
