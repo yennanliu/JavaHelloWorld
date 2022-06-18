297b92229246f854f3211f28c7f4afea5e6a1cff
yennanliu <f339339@gmail.com>
2022-06-18 19:08:34 +0800

    update demo6

--- src/main/java/Advances/Reflection/demo6.java
+++ src/main/java/Advances/Reflection/demo6.java
@@ -1,10 +1,12 @@
 package Advances.Reflection;
 
 // https://www.youtube.com/watch?v=A4oHXSAlmFg&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=652
+// https://www.youtube.com/watch?v=1cZzR0lmaPo&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=652
 
 import org.junit.jupiter.api.Test;
-
 import java.lang.reflect.Constructor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 public class demo6 {
 
@@ -31,6 +33,50 @@ public class demo6 {
         for (Constructor c : declaredConstructors){
             System.out.println(c);
         }
+    }
+
+    /**
+     *  get parent class from runtime class
+     */
+    @Test
+    public void test2(){
+
+        Class clazz = Person2.class;
+
+        Class superclass = clazz.getSuperclass();
+
+        System.out.println(">>> superclass = " + superclass);
+    }
+
+    /**
+     *  get parent class (with generic type) from runtime class
+     */
+    @Test
+    public void test3(){
+
+        Class clazz = Person2.class;
+
+        Type genericSuperclass = clazz.getGenericSuperclass();
+
+        System.out.println(">>> genericSuperclass = " + genericSuperclass);
+    }
+
+    /**
+     *  get parent class (with generic type)'s  generic type from runtime class
+     */
+    @Test
+    public void test4(){
+
+        Class clazz = Person2.class;
+
+        Type genericSuperclass = clazz.getGenericSuperclass();
+
+        // get ParameterizedType (implement 強轉 as well)
+        ParameterizedType paramType = (ParameterizedType) genericSuperclass;
+        // get genetic type
+        Type[] actualTypeArguments = paramType.getActualTypeArguments();
+
+        System.out.println(">>> actualTypeArguments = " + actualTypeArguments[0]);
     }
 
 }
