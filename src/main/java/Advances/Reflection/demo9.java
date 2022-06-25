993200739131624eedfb28c717c08fec40b920b8
yennanliu <f339339@gmail.com>
2022-06-25 16:59:51 +0800

    update demo9

--- src/main/java/Advances/Reflection/demo9.java
+++ src/main/java/Advances/Reflection/demo9.java
@@ -1,9 +1,11 @@
 package Advances.Reflection;
 
 // https://www.youtube.com/watch?v=Y2vkfcQZ8Es&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=655
+// https://www.youtube.com/watch?v=nJOZw59z4WI&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=657
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class demo9 {
@@ -33,10 +35,30 @@ public class demo9 {
         System.out.println("====== How to call static method ? =====");
 
         Method showDest = clazz.getDeclaredMethod("showDest");
+        // make sure accessible
         showDest.setAccessible(true);
         // if there is no return val, res will be null
         Object res2 = showDest.invoke(Person2.class);
         System.out.println(">>> res2 = " + res2);
+    }
+
+    /** call running class defined constructor */
+    @Test
+    public void test2() throws Exception {
+
+        Class clazz = Person2.class;
+
+        // 1)  get constructor
+        // pattern : clazz.getDeclaredConstructor(class-type)
+        Constructor constructor = clazz.getDeclaredConstructor(String.class);
+
+        // 2) make sure accessible
+        constructor.setAccessible(true);
+
+        // 3) create running class instance
+        Person2 p2 = (Person2) constructor.newInstance("jack");
+        System.out.println(">>> p2 = " + p2);
+
     }
 
 }
