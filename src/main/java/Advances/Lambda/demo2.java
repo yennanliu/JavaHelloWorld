40b0808eb42f4367bff52f2452d30d7d4a5e9467
yennanliu <f339339@gmail.com>
2022-07-02 16:44:23 +0800

    update demo2

--- src/main/java/Advances/Lambda/demo2.java
+++ src/main/java/Advances/Lambda/demo2.java
@@ -1,10 +1,12 @@
 package Advances.Lambda;
 
 // https://www.youtube.com/watch?v=m_1FoxG9EvY&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=667
+// https://www.youtube.com/watch?v=zBCTy62MTfU&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=669
 
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.function.Consumer;
 
 /**
@@ -21,6 +23,8 @@ import java.util.function.Consumer;
  *      -> form 1) No param, No return value
  *      -> form 2) need one param, No return value
  *      -> form 3) omit data type, use type inference (類型推斷)
+ *      -> form 4) if only one param, param bracket can be omitted
+ *      -> form 5) two or more params, have return value
  *
  *   4) (in java only) lambda essence : as interface instance
  *
@@ -102,6 +106,54 @@ public class demo2 {
         int[] arr1 = new int[]{1,2,3};
         // is same as below
         int[] arr2 = {1,2,3}; // type reference
+    }
+
+    /** form 4) if only one param, param bracket can be omitted */
+    @Test
+    public void test4(){
+
+        // v1 : lambda expression
+        Consumer<String> con2 = (s) -> { // omit s's type
+            System.out.println(s);
+        };
+
+        con2.accept("zzzz");
+        System.out.println("================");
+
+        // v2 : lambda expression II
+        Consumer<String> con3 = s -> { // omit s's type
+            System.out.println(s);
+        };
+    }
+
+    /** form 5) two or more params, have return value */
+    @Test
+    public void test5(){
+
+        // v1 : tradition style
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                System.out.println(o1);
+                System.out.println(o2);
+                return o1.compareTo(o2);
+            }
+        };
+
+        System.out.println(com1.compare(100, 200));
+
+        System.out.println("=================");
+
+        // v2 : lambda style
+        Comparator<Integer> com2 = (o1, o2) -> {
+
+            System.out.println(o1);
+            System.out.println(o2);
+            return o1.compareTo(o2);
+        };
+
+        System.out.println(com2.compare(-1, -2));
+
     }
 
 }
