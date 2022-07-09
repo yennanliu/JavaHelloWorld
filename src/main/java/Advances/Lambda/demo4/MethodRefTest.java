90ec0df1ecf8b31f89220a10722186e3132463c8
yennanliu <f339339@gmail.com>
2022-07-09 20:02:52 +0800

    update MethodRefTest

--- src/main/java/Advances/Lambda/demo4/MethodRefTest.java
+++ src/main/java/Advances/Lambda/demo4/MethodRefTest.java
@@ -1,11 +1,14 @@
 package Advances.Lambda.demo4;
 
 // https://www.youtube.com/watch?v=dushR-wm_E8&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=672
+// https://www.youtube.com/watch?v=jVfmtHhrKgA&list=PLmOn9nNkQxJH0qBIrtV6otI0Ep4o2q67A&index=672
 
 import org.junit.jupiter.api.Test;
 
 import java.io.PrintStream;
+import java.util.Comparator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -56,7 +59,7 @@ public class MethodRefTest {
     }
 
     /**
-     *   Example 2 :
+     *   Example 2 :  obj :: instance-method
      *
      *    -> Supplier's T get()
      *    -> Employee's String getName()
@@ -77,14 +80,51 @@ public class MethodRefTest {
         System.out.println(sup2.get());
     }
 
+    /**
+     *   Example 3 : class :: static-method
+     *
+     *    -> Comparator's int compare(T t1, T t2)
+     *    -> Integer's int compare(T t1, T t2)
+     */
     @Test
     public void test3(){
 
+        // V1 : lambda expression
+        Comparator<Integer> com1 = (t1, t2) -> Integer.compare(t1, t2);
+        System.out.println(com1.compare(1,100));
+
+        System.out.println("==================");
+
+        // V2 : Method reference
+        Comparator<Integer> com2 = Integer :: compare; // NOTE : we DON'T need to have param (t1, t2) here, since the method we use here is AS SAME AS the lambda expression it refers
+        System.out.println(com2.compare(1,100));
     }
 
+    /**
+     *   Example 4
+     *    -> Function's R apply(T t)
+     *    -> Math's Long round(Double d)
+     */
     @Test
     public void test4(){
 
+        // V0 : tradition method
+        Function<Double, Long> func0 = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double d) {
+                return Math.round(d);
+            }
+        };
+
+        // V1 : lambda expression
+        Function<Double, Long> func1 = d -> Math.round(d);
+        System.out.println(func1.apply(10.01));
+
+        System.out.println("==================");
+
+        // V2 : Method reference
+        Function<Double, Long> func2 = Math :: round;
+        System.out.println(func2.apply(10.01));
     }
 
     @Test
