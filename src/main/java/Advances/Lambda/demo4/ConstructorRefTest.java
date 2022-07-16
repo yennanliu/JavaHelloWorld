f0eb75aeb6184a18f766df0cfa448453a703a091
yennanliu <f339339@gmail.com>
2022-07-16 20:03:27 +0800

    update ConstructorRefTest.java

--- src/main/java/Advances/Lambda/demo4/ConstructorRefTest.java
+++ src/main/java/Advances/Lambda/demo4/ConstructorRefTest.java
@@ -4,6 +4,7 @@ package Advances.Lambda.demo4;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -12,8 +13,12 @@ import java.util.function.Supplier;
  *   Constructor Ref demo 1
  *
  *     1)  Constructor ref
+ *        -> similar as "method ref"
+ *        -> lambda expression abstract method's args have to be AS SAME AS Constructor's args
+ *        -> lambda expression return type is SAME as Constructor's type
  *
  *     2) array ref
+ *        -> recognize array as a "special class", so use case is similar as "constructor ref"
  */
 
 public class ConstructorRefTest {
@@ -53,7 +58,7 @@ public class ConstructorRefTest {
     public void test2(){
 
         // V1 : lambda expression
-        Function<Integer, Employee> func1 = id -> new Employee();
+        Function<Integer, Employee> func1 = id -> new Employee(id);
         Employee emp1 = func1.apply(1001);
         System.out.println(emp1);
 
@@ -79,13 +84,26 @@ public class ConstructorRefTest {
         System.out.println(func1.apply(99, "lynn"));
 
         // V2 : constructor ref
-
+        BiFunction<Integer, String, Employee> func2 = Employee :: new;
+        System.out.println(func2.apply(99, "lynn"));
     }
 
-
+    /**
+     *   Example 4 : Array reference
+     *     -> Function's apply(T t)
+     */
     @Test
     public void test4(){
 
+        // V1 : lambda expression
+        Function<Integer, String[]> func1 = length -> new String[length];
+        String[] arr1 = func1.apply(5);
+        System.out.println(Arrays.toString(arr1));
+
+        // V2 : Array ref
+        Function<Integer, String[]> func2 = String[] :: new;
+        String[] arr2 = func1.apply(10);
+        System.out.println(Arrays.toString(arr2));
     }
 
 }
