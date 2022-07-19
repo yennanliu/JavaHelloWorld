05111eb43e9c1445cf0055da7ed53eae09301952
yen.liu <yen.l@binance.com>
2022-07-19 18:39:12 +0800

    add timezone demo

---
+++ src/main/java/Basics/TimeZone/demo1.java
@@ -0,0 +1,38 @@
+package Basics.TimeZone;
+
+// https://mkyong.com/java/java-display-list-of-timezone-with-gmt/
+
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+public class demo1 {
+
+    public static void main(String[] args) {
+
+        String[] ids = TimeZone.getAvailableIDs();
+        for (String id : ids) {
+            System.out.println(displayTimeZone(TimeZone.getTimeZone(id)));
+        }
+
+        System.out.println("\nTotal TimeZone ID " + ids.length);
+
+    }
+
+    private static String displayTimeZone(TimeZone tz) {
+
+        long hours = TimeUnit.MILLISECONDS.toHours(tz.getRawOffset());
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(tz.getRawOffset()) - TimeUnit.HOURS.toMinutes(hours);
+        // avoid -4:-30 issue
+        minutes = Math.abs(minutes);
+
+        String result = "";
+        if (hours > 0) {
+            result = String.format("(GMT+%d:%02d) %s", hours, minutes, tz.getID());
+        } else {
+            result = String.format("(GMT%d:%02d) %s", hours, minutes, tz.getID());
+        }
+
+        return result;
+    }
+
+}
