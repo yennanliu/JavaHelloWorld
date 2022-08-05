a422a19eeb4f7617d24af0b972c6c9c0c4bfbb7b
yen.liu <yen.l@binance.com>
2022-08-05 10:51:38 +0800

    add freemarker dep, demo, test files

---
+++ src/main/java/Others/FreeMarker/User.java
@@ -0,0 +1,29 @@
+package Others.FreeMarker;
+
+public class User {
+    private String name;
+
+    private String location;
+
+    public User(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
---
+++ src/main/java/Others/FreeMarker/demo1.java
@@ -0,0 +1,47 @@
+package Others.FreeMarker;
+
+// http://blog.appx.tw/2017/05/10/freemarker1/
+
+import freemarker.template.TemplateException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import freemarker.template.Configuration;
+
+public class demo1 {
+
+    @Test
+    public void test1(){
+
+        Path currentRelativePath = Paths.get("");
+        String path = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is: " + path);
+
+        //初始化freemarker設定
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_26);
+        //指定樣版所在位置 (這裡是classpath的templates資料夾底下)
+        cfg.setClassForTemplateLoading(demo1.class, "/templates");
+        cfg.setDefaultEncoding("UTF-8");
+
+        //封裝資料的物件，可以是一般物件，也可以是List, Map 等集合物件
+        User user = new User("Terry", "Taiwan");
+
+        //取得樣版處理後輸出到 StringWriter
+        StringWriter sw = new StringWriter();
+        try {
+            cfg.getTemplate("helloworld.ftl").process(user, sw);
+        } catch (TemplateException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //透過StringWriter的toString()取得內容，後續可以進行處理
+        System.out.println(sw);
+    }
+
+}
