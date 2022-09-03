cde560251fcb6b4151723b05ad17a5e96f8fb94e
yennanliu <f339339@gmail.com>
2022-09-03 16:53:40 +0800

    update etDefaultIfNull.java

--- src/main/java/dev/getDefaultIfNull.java
+++ src/main/java/dev/getDefaultIfNull.java
@@ -1,7 +1,8 @@
 package dev;
 
 // https://github.com/yennanliu/til
-
+// "optional" : https://github.com/yennanliu/JavaHelloWorld/blob/main/src/main/java/Advances/Optional/demo1.java
+// java ternary operator : https://yubin551.gitbook.io/java-note/basic_java_programming/operator/ternary_instanceof
 import dev.bean.Report;
 import org.junit.jupiter.api.Test;
 
@@ -29,9 +30,15 @@ public class getDefaultIfNull {
         Report r1 = new Report("xxx",null,0,2.0);
         System.out.println(">>> r1 = " + r1);
         System.out.println(">>> r1.getName() = " + r1.getName());
-        //System.out.println(">>> r1.getName() = " + r1.getName().length()); // java.lang.NullPointerException
 
-        System.out.println(Double.parseDouble(r1.getId()));
+        System.out.println();
+
+        System.out.println(">>> r1.getId() = " + r1.getId());
+
+        /** so if r1.getId() is null, we can still get default val via ternary operator */
+        String x = r1.getId() == null ? "0.0" : r1.getId();
+        System.out.println(Double.parseDouble(x));
+        System.out.println(Double.parseDouble(r1.getId() == null ? "0.0" : r1.getId()));
     }
 
 }
