10a6cdbfd207e03562daba78d379425506082950
yennanliu <f339339@gmail.com>
2022-09-05 07:29:03 +0800

    update customSoring demo

--- src/main/java/dev/CustomSortingTest.java
+++ src/main/java/dev/CustomSortingTest.java
@@ -1,6 +1,10 @@
 package dev;
 
+import dev.bean.Data;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -13,6 +17,20 @@ public class CustomSortingTest {
     @Test
     public void test1(){
 
+        Data d1 = new Data("1",null,null);
+        Data d2 = new Data("2",null,null);
+        Data d3 = new Data("3",null,null);
+
+        Data[] dataList = new Data[]{d1,d3,d2};
+
+        Arrays.stream(dataList).forEach(System.out::println);
+
+        // NOTE : in order to use Arrays.sort, we HAVE to implement compareTom method in data bean first
+        Arrays.sort(dataList);
+
+        System.out.println();
+
+        Arrays.stream(dataList).forEach(System.out::println);
     }
 
 }
---
+++ src/main/java/dev/bean/Data.java
@@ -0,0 +1,74 @@
+package dev.bean;
+
+// https://github.com/yennanliu/JavaHelloWorld/blob/main/src/main/java/Advances/ComparableComparator/Goods.java
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Data implements Comparable {
+
+    private String id;
+    private String createTime;
+    private String[] dataPoints;
+
+    public Data(String id, String createTime, String[] dataPoints) {
+        this.id = id;
+        this.createTime = createTime;
+        this.dataPoints = dataPoints;
+    }
+
+    public Data() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String[] getDataPoints() {
+        return dataPoints;
+    }
+
+    public void setDataPoints(String[] dataPoints) {
+        this.dataPoints = dataPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id='" + id + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", dataPoints=" + Arrays.toString(dataPoints) +
+                '}';
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Data){
+            Data data = (Data) o;
+            if (Integer.valueOf(this.id) > Integer.valueOf(data.id)){
+                return 1;
+            }
+            else if (Integer.valueOf(this.id) < Integer.valueOf(data.id)){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+        }
+        throw new RuntimeException("input (price) type mismatch!");
+    }
+
+}
