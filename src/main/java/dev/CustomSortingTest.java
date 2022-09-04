3a5fa839bac9d4815b4c1f8e09428bfc75649a3f
yennanliu <f339339@gmail.com>
2022-09-04 23:53:39 +0800

    add ref

--- src/main/java/dev/CustomSortingTest.java
+++ src/main/java/dev/CustomSortingTest.java
@@ -2,6 +2,12 @@ package dev;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ *
+ *  https://segmentfault.com/q/1010000041496148
+ *  https://www.jianshu.com/p/5a3361959ae7
+ */
+
 public class CustomSortingTest {
 
     @Test
