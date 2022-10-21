f6ccbe9895131e409dc4836294e4d222c192305a
yen.liu <yen.l@binance.com>
2022-10-21 15:08:41 +0800

    add commons.io dep, add copy file demo

--- src/main/java/dev/IFileUtilsTest.java
+++ src/main/java/dev/IFileUtilsTest.java
@@ -1,9 +1,12 @@
 package dev;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import utils.IFileUtils;
+import org.apache.commons.io.FileUtils;
 
 public class IFileUtilsTest {
 
@@ -17,9 +20,17 @@ public class IFileUtilsTest {
     srcFiles.add("src/main/resources/" + "test.csv");
     srcFiles.add("src/main/resources/" + "test2.csv");
     srcFiles.add("src/main/resources/" + "test1.txt");
-    
-    String destZipFiles = "src/main/resources/" + "zipEncryptFile.zip";
+
+    //String destZipFiles = "src/main/resources/" + "zipEncryptFile.zip";
+    String destZipFiles = "src/main/resources" + ".zip";
     fileUtils.encryptFile(srcFiles, destZipFiles, "123");
+  }
+
+  @Test
+  public void test2() throws IOException {
+    String srcFile = "src/main/resources/" + "test.csv";
+    String copiedFile = "src/main/resources/" + "test_copy.csv";
+    FileUtils.copyFile(new File(srcFile), new File(copiedFile));
   }
 
 }
