47bda55b2a6a30c9f9a9d627d2dc8f5509cb93b1
yennanliu <f339339@gmail.com>
2023-02-11 23:24:37 +0000

    update

--- src/main/java/MockitoDemo/MyClass.java
+++ src/main/java/MockitoDemo/MyClass.java
@@ -1,7 +1,8 @@
 package MockitoDemo;
 
 /**
- * https://stackoverflow.com/questions/8948916/mocking-logger-and-loggerfactory-with-powermock-and-mockito
+ *  - https://stackoverflow.com/questions/8948916/mocking-logger-and-loggerfactory-with-powermock-and-mockito
+ *  - https://vivifish.medium.com/java-%E5%96%AE%E5%85%83%E6%B8%AC%E8%A9%A6%E5%B7%A5%E5%85%B7-mockito-e5f0ce93579d
  *
  */
 public class MyClass {
--- src/test/java/MockitoDemo/MyClassTest.java
+++ src/test/java/MockitoDemo/MyClassTest.java
@@ -4,13 +4,18 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 /**
  *  mockito demo 1
  *      - https://github.com/yennanliu/LambdaHelloWorld/blob/master/lab2/simple-handler/src/test/java/com/yen/SimpleHandlerTest.java
  *      - https://stackoverflow.com/questions/8948916/mocking-logger-and-loggerfactory-with-powermock-and-mockito
+ *      - https://vivifish.medium.com/java-%E5%96%AE%E5%85%83%E6%B8%AC%E8%A9%A6%E5%B7%A5%E5%85%B7-mockito-e5f0ce93579d
  *      - mock no need class/method in an unit test
  */
 @ExtendWith(MockitoExtension.class)
@@ -47,13 +52,26 @@ class MyClassTest {
 
         MyClass myClass = new MyClass();
 
+        MyUtils mock = Mockito.mock(MyUtils.class);
+
         // https://www.baeldung.com/mockito-unnecessary-stubbing-exception
         lenient().when(myUtils.myPrint()).thenReturn("this is mock msg");
         Assert.assertEquals("HELLO WORLD", myClass.toUpperCase("heLLo World"));
+        // verify is method is called
+        //Mockito.verify(mock).myPrint();
     }
 
     @Test
     public void test2(){
+
+        List<Integer> mock =  Mockito.mock(List.class);
+        mock.add(1);
+        // verify is method is called
+        Mockito.verify(mock).add(1);
+    }
+
+    @Test
+    public void test3(){
         MyClass myClass = new MyClass();
         myClass.toUpperCase("heLLo World");
         System.out.println(123);
