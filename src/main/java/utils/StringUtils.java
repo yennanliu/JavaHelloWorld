a5decd7dcfad3f783e7fb99ef25eca50533ef432
yennanliu <f339339@gmail.com>
2023-05-13 21:34:19 +0100

    update StringUtils

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -3,7 +3,6 @@ package utils;
 public class StringUtils {
 
     // https://stackoverflow.com/questions/26875877/how-to-multiply-a-string-by-an-integer
-    
     private static String multiplyString(String str, int multiplier){
 
         StringBuilder sb = new StringBuilder();
@@ -13,12 +12,21 @@ public class StringUtils {
         return sb.toString();
     }
 
+    // https://stackoverflow.com/questions/49126461/java-reverse-string-method
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
 
     // test
     public static void main(String[] args) {
 
         System.out.println(multiplyString("a", 4));
         System.out.println(multiplyString("a", 0));
+
+        String x = "abc";
+        System.out.println(x);
+        System.out.println(reverseString(x));
     }
 
 }
