74e8976ad7ccace520b35952eb530f3c5038b745
yennanliu <f339339@gmail.com>
2023-11-17 14:12:03 +0800

    update TicTacToe.java

--- dev_projects/TddPlayGround/src/main/java/com/yen/TddPlayGround/TicTacTocMongo/TicTacToe.java
+++ dev_projects/TddPlayGround/src/main/java/com/yen/TddPlayGround/TicTacTocMongo/TicTacToe.java
@@ -2,5 +2,25 @@ package com.yen.TddPlayGround.TicTacTocMongo;
 
 // https://bitbucket.org/vfarcic/tdd-java-ch06-tic-tac-toe-mongo/src/solved/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
 
+import com.yen.TddPlayGround.TicTacTocMongo.mongo.TicTacToeCollection;
+
+import java.net.UnknownHostException;
+
 public class TicTacToe {
+
+    private TicTacToeCollection ticTacToeCollection;
+    protected TicTacToeCollection getTicTacToeCollection() {
+        return ticTacToeCollection;
+    }
+
+    public TicTacToe() throws UnknownHostException {
+        this(new TicTacToeCollection());
+    }
+    public TicTacToe(TicTacToeCollection collection) {
+        ticTacToeCollection = collection;
+        if (!ticTacToeCollection.drop()) {
+            throw new RuntimeException("Dropping DB collection failed");
+        }
+    }
+
 }
