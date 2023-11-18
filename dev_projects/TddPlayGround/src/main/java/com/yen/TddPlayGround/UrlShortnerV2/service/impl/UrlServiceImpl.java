c2341be1df31a78ea285c3deba244b1a05cf5d40
yennanliu <f339339@gmail.com>
2023-11-18 18:14:13 +0800

    update

--- dev_projects/TddPlayGround/src/main/java/com/yen/TddPlayGround/UrlShortnerV2/service/impl/UrlServiceImpl.java
+++ dev_projects/TddPlayGround/src/main/java/com/yen/TddPlayGround/UrlShortnerV2/service/impl/UrlServiceImpl.java
@@ -14,6 +14,7 @@ public class UrlServiceImpl implements UrlService {
 
     @Override
     public String getUrlByKey(String key) {
+
         return "https://short_url/abc";
     }
 
--- dev_projects/TddPlayGround/src/test/java/com/yen/TddPlayGround/UrlShortnerV2/service/UrlServiceTest.java
+++ dev_projects/TddPlayGround/src/test/java/com/yen/TddPlayGround/UrlShortnerV2/service/UrlServiceTest.java
@@ -18,7 +18,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 // https://www.tpisoftware.com/tpu/articleDetails/1294
 // https://hackmd.io/@KaiChen/H1R5Nq5FI
@@ -30,7 +30,7 @@ class UrlServiceTest {
     RedisConfiguration redisConfiguration;
 
 //    @InjectMocks // InjectMocks can ONLY inject class, NOT interface
-//    UrlServiceImpl urlService; // TOOD : fix this
+//    UrlServiceImpl urlService; // TODO : fix this
 
     /**
      *  Manually Create Mocks: If you want to mock an interface,
@@ -45,13 +45,27 @@ class UrlServiceTest {
     }
 
     @Test
-    public void shouldGetUrlIfKeyExist(){
+    public void shouldGetUrlIfInputNotNull(){
 
         // setup behaviour of mock instance method
         urlService = Mockito.mock(UrlService.class);
         when(urlService.getUrlByKey(anyString())).thenReturn("https://short_url/abc");
+
         //System.out.println(urlService.getUrlByKey("123"));
         assertEquals(urlService.getUrlByKey("some-key"), "https://short_url/abc");
+    }
+
+    @Test
+    public void shouldCallOnce(){
+
+        // setup behaviour of mock instance method
+        urlService = Mockito.mock(UrlService.class);
+
+        // NOTE : we call method, then verify below
+        String result = urlService.getUrlByKey("some-key");
+
+        // Verify that the method was called exactly once
+        verify(urlService, times(1)).getUrlByKey(anyString());
     }
 
 
