6c463d9034904d61e47168a783d27225ab8e3507
yennanliu <f339339@gmail.com>
2023-11-22 18:55:02 +0800

    update

--- dev_projects/multithreading/Multithreading/src/main/java/com/yen/atguigu/pool/course40/CustomThreadPoolDemo1.java
+++ dev_projects/multithreading/Multithreading/src/main/java/com/yen/atguigu/pool/course40/CustomThreadPoolDemo1.java
@@ -32,13 +32,63 @@ public class CustomThreadPoolDemo1 {
         final ThreadFactory THREAD_FACTORY = Executors.defaultThreadFactory();
         final RejectedExecutionHandler REJECT_POLICY = new ThreadPoolExecutor.AbortPolicy();
 
-        ThreadPoolExecutor x = new ThreadPoolExecutor(CORE_POOL_SIZE,
+        // init a custom thread pool
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
                 MAX_POOL_SIZE,
                 KEEP_ALIVE_TIME,
-                UNIT, BLOCKING_QUEUE,
+                UNIT,
+                BLOCKING_QUEUE,
                 THREAD_FACTORY,
-                REJECT_POLICY);
+                REJECT_POLICY
+        );
 
+        // test run
+        try{
+            // simulate 10 customers
+            for (int i = 0; i < 10; i++){
+                // run
+                threadPool.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName() + " is running ...");
+                    }
+                });
+            }
+        }catch (Exception e){
+            //System.out.println(e);
+            e.printStackTrace();
+        }finally {
+            // close down
+            threadPool.shutdown();
+        }
+
+
+        /**
+         *  Note : we may see java.util.concurrent.RejectedExecutionException occasionally
+         *         which is normal, since we set our reject policy as new ThreadPoolExecutor.AbortPolicy();
+         *         above exception may happen when number of thread is bigger than MAX_POOL_SIZE
+         *
+         *  log :
+         *
+         *  java.util.concurrent.RejectedExecutionException: Task com.yen.atguigu.pool.course40.CustomThreadPoolDemo1$1@3ac3fd8b rejected from java.util.concurrent.ThreadPoolExecutor@5594a1b5[Running, pool size = 5, active threads = 5, queued tasks = 3, completed tasks = 0]
+         * 	at java.base/java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2055)
+         * 	at java.base/java.util.concurrent.ThreadPoolExecutor.reject(ThreadPoolExecutor.java:825)
+         * 	at java.base/java.util.concurrent.ThreadPoolExecutor.execute(ThreadPoolExecutor.java:1355)
+         * 	at com.yen.atguigu.pool.course40.CustomThreadPoolDemo1.main(CustomThreadPoolDemo1.java:51)
+         * pool-1-thread-1 is running ...
+         * pool-1-thread-4 is running ...
+         * pool-1-thread-5 is running ...
+         * pool-1-thread-1 is running ...
+         * pool-1-thread-3 is running ...
+         * pool-1-thread-2 is running ...
+         * pool-1-thread-1 is running ...
+         * pool-1-thread-5 is running ...
+         *
+         *
+         *
+         *
+         */
     }
 
 }
