ba99550c7892749ec96ee6aaf4534591daeb4b3f
yennanliu <f339339@gmail.com>
2023-11-23 11:18:53 +0800

    fix controller test

--- dev_projects/TddPlayGround/src/test/java/com/yen/TddPlayGround/ParkingLot/controller/ParkingControllerTest.java
+++ dev_projects/TddPlayGround/src/test/java/com/yen/TddPlayGround/ParkingLot/controller/ParkingControllerTest.java
@@ -1,5 +1,7 @@
 package com.yen.TddPlayGround.ParkingLot.controller;
 
+import com.yen.TddPlayGround.Bank.bean.po.User;
+import com.yen.TddPlayGround.Bank.repository.UserRepository;
 import com.yen.TddPlayGround.ParkingLot.ParkingLotApp;
 import com.yen.TddPlayGround.ParkingLot.bean.ParkingLot;
 import com.yen.TddPlayGround.ParkingLot.repository.ParkingLotRepository;
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -26,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 //import static jdk.jfr.internal.jfc.model.Constraint.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -49,10 +54,13 @@ class ParkingControllerTest {
     // TODO : double check should use Autowired or MockBean ???
     //@Autowired
     // https://blog.csdn.net/json8888/article/details/121638428
-    private ParkingLotRepository parkingLotRepository;
-    public void setParkingLotRepository(ParkingLotRepository parkingLotRepository){
-        this.parkingLotRepository = parkingLotRepository;
-    }
+//    private ParkingLotRepository parkingLotRepository;
+//    public void setParkingLotRepository(ParkingLotRepository parkingLotRepository){
+//        this.parkingLotRepository = parkingLotRepository;
+//    }
+
+    @Mock
+    ParkingLotRepository parkingLotRepository;
 
     ParkingLot parkingLot_a;
 
@@ -74,19 +82,21 @@ class ParkingControllerTest {
                 .andExpect(content().string("hello !!!"));
     }
 
-//    @Test
-//    public void testGetParkingLotSize() throws Exception {
-//
-//        // mock
-//        when(parkingLotRepository.findById(anyString()))
-//                .thenReturn(Optional.ofNullable(parkingLot_a));
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .get("/parking/parkSize/p-01")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("hello !!!"));
-//
-//    }
+    @Test
+    public void testGetParkingLotSize() throws Exception {
+
+        String PARKINGLOT_ID = "p-01";
+
+        // mock // TODO : here we mock repository and service, check if it's correct / necessary
+        Mockito.when(parkingLotRepository.findById(anyString()))
+                .thenReturn(Optional.ofNullable(parkingLot_a));
+        Mockito.when(parkingService.getById(PARKINGLOT_ID)).thenReturn(parkingLot_a);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/parking/parkSize/" + PARKINGLOT_ID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("size = 2"));
+    }
 
 }
