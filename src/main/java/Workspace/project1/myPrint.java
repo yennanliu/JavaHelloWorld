82f8c2b02eb4e7644869bdaa5d79f69207eea9c7
yennanliu <f339339@gmail.com>
2023-12-05 12:59:19 +0800

    update

--- src/main/java/Workspace/project1/myPrint.java
+++ src/main/java/Workspace/project1/myPrint.java
@@ -8,11 +8,18 @@ class MyClass{
             return;
         }
         String star = "*";
+        String space = " ";
+
+        int j = 0;
         for (int i = width; i >= 0; i-=2){
-            System.out.println(star.repeat(i));
+            System.out.println(space.repeat(j) + star.repeat(i));
+            j += 1;
         }
-         for (int i = 3; i <= width; i+=2){
-             System.out.println(star.repeat(i));
+
+        j-=2;
+        for (int i = 3; i <= width; i+=2){
+            System.out.println(space.repeat(j) + star.repeat(i));
+            j -= 1;
          }
 
     }
@@ -22,8 +29,9 @@ public class myPrint {
     public static void main(String[] args) {
 
         MyClass myClass = new MyClass();
-        int width  = 5;
+        //int width  = 5;
         //int width  = 7;
+        int width  = 9;
         myClass.myPrint(width);
     }
 
