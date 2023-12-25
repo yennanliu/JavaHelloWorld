fadf77a45e57f582994bf49c59ff5628683c44d8
yennanliu <f339339@gmail.com>
2023-12-25 18:03:51 +0800

    add model, update test

--- dev_projects/ScrapingService/src/test/java/com/yen/WebScrapingTest.java
+++ dev_projects/ScrapingService/src/test/java/com/yen/WebScrapingTest.java
@@ -1,5 +1,6 @@
 package com.yen;
 
+import model.PokemonProduct;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.jsoup.Jsoup;
@@ -17,6 +18,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebScrapingTest {
 
@@ -66,6 +69,61 @@ public class WebScrapingTest {
 //            System.out.println("href = " + href);
 //        }
 
+    }
+
+
+    // https://www.zenrows.com/blog/web-scraping-java#connect-to-target-website
+    @Test
+    public void webScrapTest2(){
+
+        String URL = "https://scrapeme.live/shop";
+
+        // initializing the HTML Document page variable
+        Document doc;
+
+        List<PokemonProduct> pokemonProducts = new ArrayList<>();
+
+        try {
+            // fetching the target website
+            // doc = Jsoup.connect(URL).get();
+            doc = Jsoup
+                    .connect(URL)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+                    .header("Accept-Language", "*")
+                    .get();
+            //System.out.println("doc = " + doc);
+
+//            Elements products = doc.select("li.product");
+//            System.out.println("products = " + products);
+
+            // initializing the list of Java object to store
+            // the scraped data
+            //List<PokemonProduct> pokemonProducts = new ArrayList<>();
+
+            // retrieving the list of product HTML elements
+            Elements products = doc.select("li.product");
+
+            // iterating over the list of HTML products
+            for (Element product : products) {
+                PokemonProduct pokemonProduct = new PokemonProduct();
+
+                // extracting the data of interest from the product HTML element
+                // and storing it in pokemonProduct
+                pokemonProduct.setUrl(product.selectFirst("a").attr("href"));
+                pokemonProduct.setImage(product.selectFirst("img").attr("src"));
+                pokemonProduct.setName(product.selectFirst("h2").text());
+                pokemonProduct.setPrice(product.selectFirst("span").text());
+
+                // adding pokemonProduct to the list of the scraped products
+                pokemonProducts.add(pokemonProduct);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(">>> pokemonProducts = ");
+        System.out.println(pokemonProducts);
     }
 
 }
