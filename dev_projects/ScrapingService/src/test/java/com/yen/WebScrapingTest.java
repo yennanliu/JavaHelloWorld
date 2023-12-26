2b7b01f26ae31337b9591cfe5b73b502f7c4b52c
yennanliu <f339339@gmail.com>
2023-12-26 16:28:37 +0800

    add, update scrape test

--- dev_projects/ScrapingService/src/test/java/com/yen/WebScrapingTest.java
+++ dev_projects/ScrapingService/src/test/java/com/yen/WebScrapingTest.java
@@ -17,7 +17,9 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WebScrapingTest {
 
@@ -72,7 +74,7 @@ public class WebScrapingTest {
 
     // https://www.zenrows.com/blog/web-scraping-java#connect-to-target-website
     @Test
-    public void webScrapTest2(){
+    public void webScrapTestPokemonV1(){
 
         String URL = "https://scrapeme.live/shop";
 
@@ -142,7 +144,87 @@ public class WebScrapingTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    // https://www.zenrows.com/blog/web-scraping-java#java-web-crawling
+    // scrape https://scrapeme.live/shop/ with page limit
+    @Test
+    public void webScrapTestPokemonV2() throws IOException {
+
+        // initializing the list of Java object to store
+        // the scraped data
+        List<PokemonProduct> pokemonProducts = new ArrayList<>();
+
+        // initializing the set of web page urls
+        // discovered while crawling the target website
+        Set<String> pagesDiscovered = new HashSet<>();
+
+        // initializing the queue of urls to scrape
+        List<String> pagesToScrape = new ArrayList<>();
+        // initializing the scraping queue with the
+        // first pagination page
+        pagesToScrape.add("https://scrapeme.live/shop/page/1/");
+
+        // the number of iteration executed
+        int i = 0;
+        // to limit the number to scrape to 5
+        int limit = 5;
+
+        while (!pagesToScrape.isEmpty() && i < limit) {
+            System.out.println(">>> i = " + i);
+
+            /** help func*/
+            scrapeProductPage(pokemonProducts, pagesDiscovered, pagesToScrape);
+
+            // incrementing the iteration number
+            i++;
+        }
+
+        System.out.println("pokemonProducts.size() = " + pokemonProducts.size());
+
+        // writing the scraped data to a db or export it to a file...
+    }
+
+    private void scrapeProductPage(List<PokemonProduct> pokemonProducts,
+                                   Set<String> pagesDiscovered,
+                                   List<String> pagesToScrape ) throws IOException {
+
+
+        System.out.println(">>> (scrapeProductPage) pagesDiscovered = " + pagesDiscovered + " pagesToScrape = " + pagesToScrape);
+
+        // the current web page is about to be scraped and
+        // should no longer be part of the scraping queue
+        String url = pagesToScrape.remove(0);
+
+        pagesDiscovered.add(url);
+
+        // scraping logic omitted for brevity...
+        // fetching the target website
+        // doc = Jsoup.connect(URL).get();
+        // initializing the HTML Document page variable
+        Document doc;
+        String URL = "https://scrapeme.live/shop";
+         doc = Jsoup
+                .connect(URL)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+                .header("Accept-Language", "*")
+                .get();
+        Elements paginationElements = doc.select("a.page-numbers");
+
+        // iterating over the pagination HTML elements
+        for (Element pageElement : paginationElements) {
+            // the new link discovered
+            String pageUrl = pageElement.attr("href");
+
+            // if the web page discovered is new and should be scraped
+            if (!pagesDiscovered.contains(pageUrl) && !pagesToScrape.contains(pageUrl)) {
+                pagesToScrape.add(pageUrl);
+            }
+
+            // adding the link just discovered
+            // to the set of pages discovered so far
+            pagesDiscovered.add(pageUrl);
+        }
     }
 
 }
