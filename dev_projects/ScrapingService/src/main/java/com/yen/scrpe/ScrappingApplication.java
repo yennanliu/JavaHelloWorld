28ff5220e6474312c1b367bbed69d19d806cfdb2
yennanliu <f339339@gmail.com>
2023-12-30 17:46:06 +0800

    refactor PokemonCollectTaskV1, clean logic in main app

--- dev_projects/ScrapingService/src/main/java/com/yen/scrpe/ScrappingApplication.java
+++ dev_projects/ScrapingService/src/main/java/com/yen/scrpe/ScrappingApplication.java
@@ -19,27 +19,17 @@ public class ScrappingApplication {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("ScrappingApplication start");
         Long start = System.currentTimeMillis();
-
-        ScrapeService scrapeService = new ScrapeService();
-        PokemonCollectTaskV1 pokemonCollectTaskV1 = new PokemonCollectTaskV1();
-
-        // initializing the list of Java object to store
-        // the scraped data
-        List<PokemonProduct> pokemonProducts = new ArrayList<>();
-        // initializing the set of web page urls
-        Set<String> pagesDiscovered = new HashSet<>(); // discovered while crawling the target website
-        // initializing the queue of urls to scrape
-        List<String> pagesToScrape = new ArrayList<>();
-        // initializing the scraping queue with the
-        pagesToScrape.add("https://scrapeme.live/shop/page/1/");
 
         // to limit the number to scrape to 5
         int LIMIT = 3; //50;
 
-        pokemonCollectTaskV1.run(scrapeService, pagesToScrape, pokemonProducts, pagesDiscovered, LIMIT);
-        System.out.println("pokemonProducts.size() = " + pokemonProducts.size());
+        ScrapeService scrapeService = new ScrapeService();
+        PokemonCollectTaskV1 pokemonCollectTaskV1 = new PokemonCollectTaskV1(scrapeService);
+        pokemonCollectTaskV1.run(LIMIT);
+
+        System.out.println("pokemonProducts.size() = " + pokemonCollectTaskV1.getPokemonProducts().size());
+        System.out.println("pokemonProducts = " + pokemonCollectTaskV1.getPokemonProducts());
 
         Long end = System.currentTimeMillis();
         System.out.println("-----> Total duration = " + ( end - start));
--- dev_projects/ScrapingService/src/main/java/com/yen/scrpe/Task/PokemonCollectTaskV1.java
+++ dev_projects/ScrapingService/src/main/java/com/yen/scrpe/Task/PokemonCollectTaskV1.java
@@ -12,15 +12,77 @@ import java.util.Set;
 
 public class PokemonCollectTaskV1 {
 
-    public void run(BaseScrapeService scrapeService, List<String> pagesToScrape, List<PokemonProduct> pokemonProducts, Set<String> pagesDiscovered, int limit) throws IOException {
+    // attr
+    //int limit;
+
+    private BaseScrapeService scrapeService;
+    // initializing the list of Java object to store
+    // the scraped data
+    private List<PokemonProduct> pokemonProducts;
+    // initializing the set of web page urls
+    private Set<String> pagesDiscovered; // discovered while crawling the target website
+    // initializing the queue of urls to scrape
+    private List<String> pagesToScrape;
+
+    // constructor
+    // PokemonCollectTaskV1.run(scrapeService, pagesToScrape, pokemonProducts, pagesDiscovered, LIMIT);
+    public PokemonCollectTaskV1(){
+
+    }
+
+    public PokemonCollectTaskV1(BaseScrapeService scrapeService){
+
+        this.scrapeService = scrapeService;
+        this.pokemonProducts = new ArrayList<>();
+        this.pagesDiscovered = new HashSet<>();
+        this.pagesToScrape = new ArrayList<>();
+        // initializing the scraping queue with the
+        this.pagesToScrape.add("https://scrapeme.live/shop/page/1/");
+    }
+
+    // getter, setter
+    public BaseScrapeService getScrapeService() {
+        return scrapeService;
+    }
+
+    public void setScrapeService(BaseScrapeService scrapeService) {
+        this.scrapeService = scrapeService;
+    }
+
+    public List<PokemonProduct> getPokemonProducts() {
+        return pokemonProducts;
+    }
+
+    public void setPokemonProducts(List<PokemonProduct> pokemonProducts) {
+        this.pokemonProducts = pokemonProducts;
+    }
+
+    public Set<String> getPagesDiscovered() {
+        return pagesDiscovered;
+    }
+
+    public void setPagesDiscovered(Set<String> pagesDiscovered) {
+        this.pagesDiscovered = pagesDiscovered;
+    }
+
+    public List<String> getPagesToScrape() {
+        return pagesToScrape;
+    }
+
+    public void setPagesToScrape(List<String> pagesToScrape) {
+        this.pagesToScrape = pagesToScrape;
+    }
+
+    // method
+    public void run(int limit) throws IOException {
 
         int i  = 0;
 
-        while (!pagesToScrape.isEmpty() && i < limit) {
+        while (!this.pagesToScrape.isEmpty() && i < limit) {
             System.out.println(">>> i = " + i);
 
             /** help func*/
-            scrapeService.scrapeProductPage(pokemonProducts, pagesDiscovered, pagesToScrape, i);
+            this.scrapeService.scrapeProductPage(pokemonProducts, pagesDiscovered, pagesToScrape, i);
 
             // incrementing the iteration number
             i++;
