552e22205ed40dd515741c1f14e59f47cb1fcf4f
yennanliu <f339339@gmail.com>
2024-07-11 14:20:35 +0800

    add

--- src/main/java/Workspace/test.java
+++ src/main/java/Workspace/test.java
@@ -1,15 +1,15 @@
-package Workspace;
-
-import com.yen.core.Util;
-
-public class test {
-  public static void main(String[] args) {
-    Util util = new Util();
-    System.out.println(util.add(1, 2));
-    System.out.println(util.substract(1, 2));
-    System.out.println(util.product(1, 2));
-    System.out.println(util.division(1, 2));
-    System.out.println(util.square(10));
-    System.out.println(util.sin(10));
-  }
-}
+//package Workspace;
+//
+//import com.yen.core.Util;
+//
+//public class test {
+//  public static void main(String[] args) {
+//    Util util = new Util();
+//    System.out.println(util.add(1, 2));
+//    System.out.println(util.substract(1, 2));
+//    System.out.println(util.product(1, 2));
+//    System.out.println(util.division(1, 2));
+//    System.out.println(util.square(10));
+//    System.out.println(util.sin(10));
+//  }
+//}
