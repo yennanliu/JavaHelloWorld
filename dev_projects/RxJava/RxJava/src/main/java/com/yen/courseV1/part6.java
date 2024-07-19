5497aff979f8cdadf1587b7ac0bc241d87dc8b06
yennanliu <f339339@gmail.com>
2024-07-19 17:13:06 +0800

    update

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part6.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part6.java
@@ -2,9 +2,35 @@ package com.yen.courseV1;
 
 // https://youtu.be/Kx1S_wtwA2g?si=z4iIAVShgn9Kxvmi
 
-public class part6 {
-  public static void main(String[] args) {
+// TODO : complete this
 
+import static java.lang.Thread.sleep;
+
+import rx.Observable;
+
+public class part6 {
+  public static void main(String[] args) {}
+
+  private static void synchronousObservableExample() {
+    Observable.range(1, 1000000)
+        .map(id -> new Item(id))
+        .subscribe(
+            item -> {
+              try {
+                sleep(1000);
+              } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+              }
+              System.out.println("received Item " + item.id + "\n");
+            });
   }
-  
+}
+
+// static class ?
+class Item {
+  public Integer id;
+
+  public Item(Integer id) {
+    this.id = id;
+  }
 }
