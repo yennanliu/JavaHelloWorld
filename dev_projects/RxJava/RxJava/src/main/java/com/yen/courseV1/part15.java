b8d0a8c5aa7394d9be18d5595803115211bb1bc2
yennanliu <f339339@gmail.com>
2024-07-21 16:31:15 +0800

    add

---
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part15.java
@@ -0,0 +1,11 @@
+package com.yen.courseV1;
+
+// part 15 : Threading - observeOn()
+// https://youtu.be/9yk3vlalj3E?si=G0vJk_jI90WQlIym
+
+public class part15 {
+  public static void main(String[] args) {
+
+  }
+
+}
