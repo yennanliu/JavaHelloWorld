52c40f8f50dd99284c20b572bcda3872f8518e69
yennanliu <f339339@gmail.com>
2024-07-21 16:23:56 +0800

    add

--- dev_projects/RxJava/README.md
+++ dev_projects/RxJava/README.md
@@ -6,6 +6,11 @@
 	- https://youtu.be/l4zfIkRsT_8?si=7O7qBEwWrwGd4qnn&t=345
 <p><img src ="./doc/pic/rx_java_can_onserver.png" ></p>
 
+- Schedulers
+	- https://youtu.be/4menosDtstk?si=e6JB22qoqaSwXFQa&t=440
+<p><img src ="./doc/pic/rx_java_can_onserver.png" ></p>
+
+
 
 ## Ref
 - https://www.youtube.com/watch?v=7mbjhNCWqvs&list=PLZ3FH0lcV0117kiek3g-qiQDkO4ezy_Ro
---
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part14.java
@@ -0,0 +1,41 @@
+package com.yen.courseV1;
+
+// part 14 : Threading - subscribeOn()
+// https://youtu.be/4menosDtstk?si=G2hBbjGornntY0BT
+
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+public class part14 {
+  public static void main(String[] args) {
+
+    /** Example 1  : subscribeOn(Schedulers.computation())
+     *
+     *  1) when main thread meet ".subscribeOn(Schedulers.computation())",
+     *     it will proceed to next lines, but NOT waiting till "Observable.just(1, 2, 3, 4, 5) subscribe logic" to complete
+     *
+     */
+    Observable.just(1, 2, 3, 4, 5)
+        .doOnNext(
+            item ->
+                System.out.println(
+                    "push item " + item + " on " + Thread.currentThread().getName() + " thread"))
+        .subscribeOn(Schedulers.computation()) // // NOTE !!! with Schedulers.computation(), we NO LONGER USE main thread, but use thread such as RxComputationThreadPool-1  ...
+        .subscribe(
+            item ->
+                System.out.println(
+                    "Received item "
+                        + item
+                        + " on "
+                        + Thread.currentThread().getName()
+                        + " thread \n"));
+
+    // prevent main thread to finish
+      try{
+          Thread.sleep(10000);
+      }catch (InterruptedException e){
+          e.printStackTrace();
+      }
+
+  }
+}
