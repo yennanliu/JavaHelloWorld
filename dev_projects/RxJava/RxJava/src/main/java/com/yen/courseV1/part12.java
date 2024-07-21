411eeed96a4cae168f557608996966e235f48c90
yennanliu <f339339@gmail.com>
2024-07-21 15:50:38 +0800

    add

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part12.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part12.java
@@ -4,6 +4,8 @@ package com.yen.courseV1;
 // https://youtu.be/rhkPPFVw5Ns?si=O7c0MkjflPsPENAa
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.AsyncSubject;
+import io.reactivex.rxjava3.subjects.BehaviorSubject;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import io.reactivex.rxjava3.subjects.Subject;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class part12 {
   public static void main(String[] args) {
 
+    /** example 1 : PublishSubject */
     // create 2 sources
     Observable<Long> source1 = Observable.interval(1, TimeUnit.SECONDS);
     Observable<Long> source2 = Observable.interval(1, TimeUnit.SECONDS);
@@ -19,7 +22,7 @@ public class part12 {
     Subject<Long> subject = PublishSubject.create();
 
     // subscribe to the publishSubject object
-    subject.subscribe(item -> System.out.println(item));
+    subject.subscribe(item -> System.out.println("received item = " + item));
 
     // we will need to tell our subject that
     // it will receive items from 2 different sources
@@ -32,6 +35,33 @@ public class part12 {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
+
+    /** example 2 : BehaviorSubject */
+    Subject<Integer> subject2 = BehaviorSubject.create();
+
+    subject2.subscribe(item -> System.out.println("person 1 listens to song = " + item));
+
+    subject2.onNext(1);
+    subject2.onNext(2);
+    subject2.onNext(3);
+
+    subject2.subscribe(item -> System.out.println("person 2 listens to song = " + item));
+
+    /** example 3 : AsyncSubject */
+    Subject<Integer> subject3 = AsyncSubject.create();
+
+    subject3.subscribe(item -> System.out.println("person A listens to song = " + item));
+
+    subject3.onNext(1);
+    subject3.onNext(2);
+    subject3.onNext(3);
+
+    subject3.subscribe(item -> System.out.println("person B listens to song = " + item));
+
+    subject3.onNext(4);
+
+    subject3.onComplete();
   }
+
 
 }
---
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part13.java
@@ -0,0 +1,44 @@
+package com.yen.courseV1;
+
+// part 13 : Custom operator
+// https://youtu.be/xMY0pVrLJgY?si=WCQcYoowGEXP0oVw
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.ObservableOperator;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+import rx.Observable;
+
+public class part13 {
+  public static void main(String[] args) {
+
+    Observable.just(1, 2, 3, 4)
+        .filter(item -> item % 2 == 0)
+        .subscribe(item -> System.out.println(item));
+  }
+
+  private ObservableOperator<Integer, Integer> takeEven() {
+    // return null;
+    return new ObservableOperator<Integer, Integer>() {
+      @Override
+      public @NonNull Observer<? super Integer> apply(@NonNull Observer<? super Integer> observer)
+          throws Throwable {
+        return new DisposableObserver<Integer>() {
+          @Override
+          public void onNext(@NonNull Integer item) {
+              if(item % 2 == 0){
+                  observer.onNext(item);
+              }
+          }
+
+          @Override
+          public void onError(@NonNull Throwable e) {}
+
+          @Override
+          public void onComplete() {}
+        };
+      }
+    };
+  }
+
+}
