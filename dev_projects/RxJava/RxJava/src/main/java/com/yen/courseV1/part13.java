b4e3b1cddf5ce59bee1575dca114e84a9fe4be67
yennanliu <f339339@gmail.com>
2024-07-21 15:56:50 +0800

    add

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part13.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part13.java
@@ -4,20 +4,23 @@ package com.yen.courseV1;
 // https://youtu.be/xMY0pVrLJgY?si=WCQcYoowGEXP0oVw
 
 import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableOperator;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.observers.DisposableObserver;
-import rx.Observable;
+//import rx.Observable;
 
 public class part13 {
   public static void main(String[] args) {
 
     Observable.just(1, 2, 3, 4)
-        .filter(item -> item % 2 == 0)
+        //.filter(item -> item % 2 == 0) // V1
+        .lift(takeEven()) // V2 (do above logic with custom operator)
         .subscribe(item -> System.out.println(item));
   }
 
-  private ObservableOperator<Integer, Integer> takeEven() {
+  // custom operator
+  private static ObservableOperator<Integer, Integer> takeEven() {
     // return null;
     return new ObservableOperator<Integer, Integer>() {
       @Override
