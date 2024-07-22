e14019206c50346b68e3549ac6c30b2bf35339d4
yennanliu <f339339@gmail.com>
2024-07-22 17:40:29 +0800

    add

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part15.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/courseV1/part15.java
@@ -3,9 +3,28 @@ package com.yen.courseV1;
 // part 15 : Threading - observeOn()
 // https://youtu.be/9yk3vlalj3E?si=G0vJk_jI90WQlIym
 
+//import rx.Observable;
+//import rx.schedulers.Schedulers;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class part15 {
   public static void main(String[] args) {
 
-  }
+    /** example 1 */
+    Observable.just("hello world!")
+        .subscribeOn(Schedulers.io()) // NOTE here
+        .doOnNext(item -> System.out.println("emit item : " + Thread.currentThread().getName()))
+        .observeOn(Schedulers.single()) // NOTE here
+        .subscribe(item -> System.out.println("observe item : " + Thread.currentThread().getName()));
 
+    // prevent app from exciting
+    try {
+      Thread.sleep(100000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
 }
