87c61a652fae0b5937c209abc4a82c55c00f1708
yennanliu <f339339@gmail.com>
2024-07-23 15:34:10 +0800

    add

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/CourseV2/part15.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/CourseV2/part15.java
@@ -3,6 +3,14 @@ package com.yen.CourseV2;
 // course 15
 // https://youtu.be/WbxFxEO1fos?si=YJTqNYDyFz6bJr6k
 
+/**
+ *  - Flux
+ *  - Mono
+ *
+ */
+
 public class part15 {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+
+  }
 }
