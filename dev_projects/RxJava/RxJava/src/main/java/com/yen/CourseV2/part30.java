6b4f14f4b9f2ddd40027d17248449369f88755b8
yennanliu <f339339@gmail.com>
2024-07-25 14:50:15 +0800

    add

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/CourseV2/part30.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/CourseV2/part30.java
@@ -2,6 +2,9 @@ package com.yen.CourseV2;
 
 // course 30 : Error handling in reactive programming
 // https://youtu.be/xzn2KbmkcPE?si=0ijT9sQJnfEpEHq3
+
+import reactor.core.publisher.Flux;
+
 public class part30 {
   public static void main(String[] args) {
 
@@ -10,6 +13,10 @@ public class part30 {
      *  NOTE !!!
      *
      *   compare below difference (V1, V2, V3)
+     *
+     *   - err in subscribe
+     *   - only subscribe, no err
+     *   - doOnError
      *
      */
     ReactiveSources.intNumbersFluxWithException()
@@ -28,10 +35,22 @@ public class part30 {
         );
 
 
-    /** Example 2 */
+    /** Example 2
+     *
+     *  onErrorContinue
+     */
     ReactiveSources.intNumbersFluxWithException()
               .onErrorContinue((e, item) -> System.out.println("error !!!" + e.getMessage()))
               .subscribe(num -> System.out.println(num));
+
+    /**
+     * Example 3
+     *
+     * onErrorResume
+     */
+    ReactiveSources.intNumbersFluxWithException()
+        .onErrorResume(err -> Flux.just(-1,-2))
+        .subscribe(num -> System.out.println(num));
 
     // prevent app from exciting
       try {
