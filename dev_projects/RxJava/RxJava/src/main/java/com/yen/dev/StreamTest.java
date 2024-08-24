69b6af462f8f559de6b6d4a36f9699ace91b068e
yennanliu <f339339@gmail.com>
2024-08-24 17:32:55 +0800

    update StreamTest

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/dev/StreamTest.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/dev/StreamTest.java
@@ -1,10 +1,13 @@
 package com.yen.dev;
 
+import com.yen.workspace.ReportUtil;
+import com.yen.workspace.model.Report;
 import io.reactivex.rxjava3.core.Flowable;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *  https://blog.csdn.net/xiaofeng10330111/article/details/130457788
@@ -15,6 +18,28 @@ public class StreamTest {
 
     @Test
     public void test1() {
+
+        // from workspace
+        List<Report> reports = ReportUtil.getReports();
+    reports.stream()
+        .flatMap(
+            report -> {
+
+              // >>> stream = java.util.stream.ReferencePipeline$Head@5b94b04d
+              System.out.println(">>> stream = " + report.getResults().stream());
+
+              // >>> stream str = java.util.stream.ReferencePipeline$Head@8c3b9d
+              System.out.println(">>> stream str = " + report.getResults().stream().toString());
+
+              // >>> stream array = [Ljava.lang.Object;@e4487af
+              System.out.println(">>> stream array = " + report.getResults().stream().toArray());
+
+              // >>> stream first = Optional[TestResult{id=4, serialNum='s1', status='ok', createTime=Sat Aug 24 17:31:52 CST 2024}]
+              System.out.println(">>> stream first = " + report.getResults().stream().findFirst().toString());
+
+              return report.getResults().stream();
+            })
+        .collect(Collectors.toList());
     }
 
 }
