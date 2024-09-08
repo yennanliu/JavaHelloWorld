749dec80f8760b0f14b406f4e3d3d67e7e3c196b
yennanliu <f339339@gmail.com>
2024-09-08 18:54:29 +0800

    add

---
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/dev/FromCallableTest.java
@@ -0,0 +1,22 @@
+package com.yen.dev;
+
+import io.reactivex.rxjava3.core.Single;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FromCallableTest {
+
+    @Test
+    public void test1(){
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        //Single.fromCallable(myList);
+    }
+
+}
