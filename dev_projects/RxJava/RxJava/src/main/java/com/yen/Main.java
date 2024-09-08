ab03f76d43f65d8f584a9d3ec7593a7d77e02f13
yennanliu <f339339@gmail.com>
2024-09-08 18:40:12 +0800

    update

--- dev_projects/RxJava/RxJava/src/main/java/com/yen/Main.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/Main.java
@@ -6,8 +6,15 @@ public class Main {
   public static void main(String[] args) {
 
     System.out.println("RX Hello world!");
+    //
+    //    System.out.println(FileUtil.getFileList(""));
+    //    System.out.println(FileUtil.getFileList("src/main/java/com/yen/dev"));
 
-    System.out.println(FileUtil.getFileList(""));
-    System.out.println(FileUtil.getFileList("src/main/java/com/yen/dev"));
+    FileUtil.getFileListRx("src/main/java/com/yen/dev")
+        .subscribe(
+            file -> {
+              System.out.println("file = " + file);
+            });
   }
+
 }
--- dev_projects/RxJava/RxJava/src/main/java/com/yen/Util/FileUtil.java
+++ dev_projects/RxJava/RxJava/src/main/java/com/yen/Util/FileUtil.java
@@ -1,8 +1,7 @@
 package com.yen.Util;
 
-import reactor.core.publisher.Flux;
-
-import java.io.IOException;
+import io.reactivex.rxjava3.core.Single;
+import java.nio.file.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,23 +10,45 @@ import java.util.stream.Collectors;
 
 public class FileUtil {
 
-    // list file under path
-    // https://www.baeldung.com/java-list-directory-files
-    public static List<Path> getFileList(String dir) {
-        System.out.println("dir = " + dir);
-        List<Path> fileList = null;
-        try{
-            fileList = Files.list(Paths.get(dir))
-                    .filter(file -> !Files.isDirectory(file))
-                    .map(file -> {
-                        System.out.println("file = " + file + " filename = " + file.getFileName());
-                        return file;
-                    }).collect(Collectors.toList());
-        }catch (Exception e){
-            System.out.println("get file list exception : " + e.getMessage());
-        }
-        return fileList;
+  // list file under path
+  // https://www.baeldung.com/java-list-directory-files
+  public static List<Path> getFileList(String dir) {
+    System.out.println("dir = " + dir);
+    List<Path> fileList = null;
+    try {
+      fileList =
+          Files.list(Paths.get(dir))
+              .filter(file -> !Files.isDirectory(file))
+              .map(
+                  file -> {
+                    System.out.println("file = " + file + " filename = " + file.getFileName());
+                    return file;
+                  })
+              .collect(Collectors.toList());
+    } catch (Exception e) {
+      System.out.println("get file list exception : " + e.getMessage());
     }
+    return fileList;
+  }
 
+  public static Single<List<Path>> getFileListRx(String dir) {
+    System.out.println("(RX) dir = " + dir);
 
+    return Single.fromCallable(
+            () ->
+                Files.list(Paths.get(dir))
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(
+                        file -> {
+                          System.out.println(
+                              "file = " + file + " filename = " + file.getFileName());
+                          return file;
+                        })
+                    .collect(Collectors.toList()))
+        .onErrorReturn(
+            e -> {
+              System.out.println("(RX) get file list exception: " + e.getMessage());
+              return List.of(); // return an empty list if an error occurs
+            });
+  }
 }
