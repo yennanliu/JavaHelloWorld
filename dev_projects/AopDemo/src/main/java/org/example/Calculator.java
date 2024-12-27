f795bb2613cf8fbe71a98d4a4fb28b1664e9c280
yennanliu <f339339@gmail.com>
2024-12-27 17:57:44 +0800

    add

---
+++ dev_projects/AopDemo/src/main/java/org/example/Calculator.java
@@ -0,0 +1,9 @@
+package org.example;
+
+// https://youtu.be/U40ZiGNKmiI?si=f5OLDwVTKYnwCJVl
+public interface Calculator {
+    int add(int i, int j);
+    int sub(int i, int j);
+    int mul(int i, int j);
+    int div(int i, int j);
+}
---
+++ dev_projects/AopDemo/src/main/java/org/example/CalculatorImpl.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class CalculatorImpl implements Calculator{
+    @Override
+    public int add(int i, int j) {
+        System.out.println("--> before");
+        int res = i + j;
+        System.out.println("--> after");
+        return res;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        System.out.println("--> before");
+        int res = i - j;
+        System.out.println("--> after");
+        return res;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        System.out.println("--> before");
+        int res = i * j;
+        System.out.println("--> after");
+        return res;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        if (j == 0){
+            return 0;
+        }
+        System.out.println("--> before");
+        int res = i / j;
+        System.out.println("--> after");
+        return res;
+    }
+}
